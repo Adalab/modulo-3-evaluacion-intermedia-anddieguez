244d3c589ff5052c7609282d98266c8d777c9f54
anddieguez <andreadiegar@gmail.com>
2023-10-16 10:30:24 +0200

    ej1

---
+++ src/components/App.jsx
@@ -0,0 +1,63 @@
+//imports dependencias, imagenes, componentes, estilos
+import reactLogo from "../images/react.svg";
+import viteLogo from "/vite.svg";
+import "../styles/App.scss";
+
+// Fichero src/components/App.jsx
+// Fichero src/components/App.jsx
+// Fichero src/components/App.jsx
+import { useEffect, useState } from "react";
+
+function App() {
+
+const [countriesList, setCountriesList] = useState ([]);
+
+
+useEffect(()=> {
+  fetch("https://restcountries.com/v3.1/all?fields=name,capital,flag,continents")
+  .then((response)=>response.json())
+  .then((dataApi)=>{
+    setCountriesList(dataApi)
+    console.log(dataApi)
+  })
+}, [])
+
+const paintContruntries = () => {
+  return countriesList.map((eachCountry)=><li className="country_item"><p>{eachCountry.flag}</p><h3>{eachCountry.name.common}</h3><p>{eachCountry.capital}</p><p>{eachCountry.continents}</p></li>)
+}
+
+ //const listLi = namesList.map((name, index) => <li key={index}>{name}</li>);  o en constante o en funcion
+
+  // Funciones de renderizado
+
+
+
+  return (
+  
+  <>
+  <h2>Filters</h2>
+  <form >
+    <input
+    type="text"
+    placeholder="Spain..."
+    
+    />
+  </form>
+
+  <select className="select">
+      <option value="">All</option>
+      <option className="" value="">Africa</option>
+      <option className="" value="">North America</option>
+      <option className="" value="">South America</option>
+      <option className="" value="">Asia</option>
+      <option className="" value="">Europe</option>
+      <option className="" value="">Oceania</option>
+    </select>
+  <ul>
+   {paintContruntries()}
+  </ul>
+  </>
+  );
+}
+
+export default App;
